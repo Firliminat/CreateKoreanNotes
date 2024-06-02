8c04083b3b22cc5d1615a78a708eda34b1d1ade7
Alexandre BOBET <bobet.alexandre@gmail.com>
2024-06-02 22:08:42 +0900

    Feat: add tags to duplicate Kr or Fr notes

--- main.py
+++ main.py
@@ -6,13 +6,6 @@ from dict_api import get_note_from_korean
 
 DEBUG = True
 
-# notes, header_lines = from_csv_file('ExampleData/Coréen__Nouveau_Vocabulaire.txt')
-# to_csv_file(
-#   notes,
-#   'TestOutput/Coréen__Vocabulaire.txt',
-#   header_lines
-# )
-
 def get_korean_words_list(file_name: str) -> list[str]:
   words: list[str] = []
   with open(file_name, newline='\n') as csv_file:
@@ -27,7 +20,10 @@ def get_korean_words_list(file_name: str) -> list[str]:
   
   return words
 
-korean_words = get_korean_words_list('./TestInput/NewWords.txt')
+
+existing_deck, header_lines = Deck.from_csv_file('./TestInput/Coréen__Vocabulaire_Existing.txt')
+
+new_korean_words = get_korean_words_list('./TestInput/NewWords.txt')
 
 errors_csv_file = open('./TestOutput/Errors.txt', 'w', newline='\n')
 errors_writer = csv.writer(
@@ -38,16 +34,24 @@ errors_writer = csv.writer(
 )
 errors: list[Exception] = []
 new_deck = Deck()
-for word in tqdm(korean_words):
+for word in tqdm(new_korean_words):
   try:
     new_note = get_note_from_korean(word, True, './TestOutput/Media')
+
+    # Tagging duplicates
+    if existing_deck.has_korean(new_note) or new_deck.has_korean(new_note):
+      new_note.tags += 'KrDup' + ', '
+    if existing_deck.has_french(new_note) or new_deck.has_french(new_note):
+      new_note.tags += 'FrDup' + ', '
+    new_note.tags = new_note.tags.removesuffix(', ')
+
     new_deck.update(new_note)
   except Exception as e:
     errors_writer.writerow([word, repr(e)])
     errors.append(e)
 errors_csv_file.close()
 
-new_deck.to_csv_file('TestOutput/Coréen__Vocabulaire_new.txt')
+new_deck.to_csv_file('TestOutput/Coréen__Vocabulaire_new.txt', header_lines)
 
 if DEBUG and len(errors) > 0:
   raise Exception(errors)
